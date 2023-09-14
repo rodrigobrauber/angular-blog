405c0de27d2d1ba478182d800a92711ad84a33b8
Rodrigo Rauber <rodriigorauber@gmail.com>
2023-09-14 20:14:50 -0300

    Adicionando rotas, deixando tudo dinamico e finalizando projeto!

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -10,6 +10,8 @@ export class SmallCardComponent implements OnInit{
   cardImage:string = ""
   @Input()
   cardTitle:string = ""
+  @Input()
+  Id:string = "0"
 
   constructor(){
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Novo Homem de ferro em 3D",
+    "description": "Marvel Studio anuncia novo filme do homim de ferro",
+    "photoCover":"https://wallpapers.com/images/hd/iron-man-pictures-k83lqhe6vnh95b1m.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Saiu nova versao do Angular",
+    "description": "Saiu nova versao do Angular",
+    "photoCover":"https://miro.medium.com/v2/resize:fit:1358/0*wuNf24urnMp7ypDp.png"
+  },
+  {
+    "id": "3",
+    "title": "Melhore suas habilidades no javascript",
+    "description": "Melhore suas habilidades no javascript",
+    "photoCover":"https://www.computerhope.com/jargon/j/javascript.png"
+  },
+  {
+    "id": "4",
+    "title": "Saiba tudo sobre a polêmica envolvendo Ruby on Rails",
+    "description": "olaaa",
+    "photoCover":"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190902124355/ruby-programming-language.png"
+  }
+]
